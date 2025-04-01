636dfb68316a6527115debfcc128e1099cabbc5a
Marc <mmarclawrence@gmail.com>
2025-04-01 21:36:27 +0800

    Magadan

---
+++ _middleware/validate-request.js
@@ -0,0 +1,18 @@
+const { allow } = require("joi");
+
+module.exports = validateRequest;
+
+function validateRequest(req, res, schema){
+  const options = {
+    abortEarly: false, // include all errors
+    allowUnknown: false, // ignore unknown props
+    stripUnknown: true // remove unknown props
+  }
+  const { error, value } = schema.validate(req.body, options);
+  if (error){
+    next(`Validation error: ${error.details.map(x => x.message).join(', ')}`);
+  } else {
+    req.body = value;
+    next();
+  }
+}
